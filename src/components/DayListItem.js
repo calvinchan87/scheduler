5af186356f4a779aac2af25b9859bbfc40c4e000
Calvin <calvinchan87@gmail.com>
2021-05-11 16:01:45 -0700

    styling the dayListItem, create unit tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,34 @@
 import React from "react";
+import "components/DayListItem.scss";
+
+const classNames = require('classnames');
 
 export default function DayListItem(props) {
 
+  let dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  let formatSpots = function() {
+    let spotsString = "no spots remaining";
+    if (props.spots > 1) {
+      spotsString = `${props.spots} spots remaining`
+    }
+    if (props.spots === 1) {
+      spotsString = "1 spot remaining"
+    }
+    return spotsString;
+  };
+
   return (
     <li
+      className={dayClass}
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
