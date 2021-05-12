348b37346d0bc845a1658f0d15a61e1232bca2dd
Calvin <calvinchan87@gmail.com>
2021-05-12 12:36:09 -0700

    refactor interview componentsand update stories

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,7 +4,7 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList(props){
 
-  const interviewer = props.interviewers.map(interviewer => {
+  const interviewers = props.interviewers.map(interviewer => {
 
     return (
       <InterviewerListItem
@@ -12,7 +12,7 @@ export default function InterviewerList(props){
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        setInterviewer={event => props.setInterviewer(interviewer.id)}
       />
     )
   })
@@ -20,7 +20,7 @@ export default function InterviewerList(props){
     return (
       <section className="interviewers">
         <h4 className="interviewers__header text--light">Interviewer</h4>
-        <ul className="interviewers__list">{interviewer}</ul>
+        <ul className="interviewers__list">{interviewers}</ul>
       </section>
     )
   }
