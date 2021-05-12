62c8b647d9b72b710edc2a561fae1755195e9a46
Calvin <calvinchan87@gmail.com>
2021-05-12 06:50:20 -0700

    build, style, narrate InterviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+
+const classNames = require('classnames');
+
+export default function InterviewerListItem(props) {
+
+  let interviewerClass = classNames({
+    "interviewers__item": true,
+    "interviewers__item-image": props.avatar && !props.selected,
+    "interviewers__item--selected": props.selected && !props.avatar,
+    "interviewers__item-image--selected": props.avatar && props.selected
+  })
+
+  return (
+    <li
+      className={interviewerClass}
+      onClick={props.setInterviewer}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
