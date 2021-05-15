38f7021470c34e9739b4f7ac9bd20b3db9072b42
Calvin <calvinchan87@gmail.com>
2021-05-15 15:06:17 -0700

    Building a Hook: back, back limit, transition with replace, Show.js bug fix

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,10 +3,29 @@ import React, { useState } from "react";
 export default function useVisualMode(initial) {
 
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = newMode => setMode(newMode);
+  const transition = function(newMode, replace = false) {
 
-  return { mode, transition };
+    if (replace) {
+      let newHistory = history.slice(0, -1);
+      newHistory.push(newMode);
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    } else {
+      history.push(newMode);
+      setMode(newMode);
+    }
+  }
+
+  const back = function() {
+    if (history.length >= 2) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  };
+
+  return { mode, transition, back };
 }
 
 // export default function useVisualMode(startingMode) {
