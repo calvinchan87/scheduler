2bf74a26db467ca610994ba903f5288dd57db97b
Calvin <calvinchan87@gmail.com>
2021-05-18 12:22:34 -0700

    Validating props, component test template

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
-export default function InterviewerList(props){
+function InterviewerList(props){
 
   const interviewers = props.interviewers.map(interviewer => {
 
@@ -17,10 +18,16 @@ export default function InterviewerList(props){
     )
   })
 
-    return (
-      <section className="interviewers">
-        <h4 className="interviewers__header text--light">Interviewer</h4>
-        <ul className="interviewers__list">{interviewers}</ul>
-      </section>
-    )
-  }
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  )
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
